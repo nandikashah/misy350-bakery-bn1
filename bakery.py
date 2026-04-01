9878910eda2fde0243f47b655555556f8441066b
Nandika <nandika@udel.edu>
2026-04-01 11:02:19 -0400

    new

---
+++ bakery.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import json
+from pathlib import Path
+from datetime import datetime
+import uuid
+import time
+
+st.set_page_config(page_title="Blossom and Nandika's Bakery Delights", layout="centered")
+
+st.title("Blossom and Nandika's Bakery Delights")
+
+
+
